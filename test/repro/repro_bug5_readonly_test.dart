87b59cba60d41ea0275f2a37a12874ef8695cee1
furukawa.masaki <10431012+moezakura@users.noreply.github.com>
2026-08-15 23:42:16 +0900

    fix: add l10n delegates to bug5 repro test scaffold

--- test/repro/repro_bug5_readonly_test.dart
+++ test/repro/repro_bug5_readonly_test.dart
@@ -19,6 +19,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'package:flutter_muxpod/l10n/app_localizations.dart';
 import 'package:flutter_muxpod/providers/connection_provider.dart';
 import 'package:flutter_muxpod/screens/connections/connection_form_screen.dart';
 import 'package:flutter_muxpod/services/keychain/secure_storage.dart';
@@ -70,6 +71,8 @@ Future<_TestSshClient> _pumpForm(WidgetTester tester) async {
         ),
       ],
       child: MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
         home: ConnectionFormScreen(connectionId: null),
       ),
     ),
